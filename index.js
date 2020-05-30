058ed7d3890218a7eb284f76b4c1556781131637
Yuri Godke <yurigodke@gmail.com>
2020-05-30 16:11:29 -0300

    Adds react

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = document.getElementById("app");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./pages";
+
+ReactDOM.render(<App></App>, document.getElementById("app"));
---
+++ pages/login/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class Login extends PureComponent {
+  render() {
+    return <div>Login</div>;
+  }
+}
+
+export default Login;
